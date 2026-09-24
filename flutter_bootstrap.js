cd9ab2335267563cc3773e099fb9bfa66ec9a6c5
BC201-Fireapp <Chuckstrader@Yahoo.com>
2026-09-24 07:35:10 -0400

    Publish Locus 7832ccf remove ignition-source survey UI.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3417136588" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "28631787" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
